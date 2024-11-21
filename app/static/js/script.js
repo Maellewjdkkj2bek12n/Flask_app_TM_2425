81d2612cc7a8515d9922bdba2a70cb1e344e3379
MaelleG <182913177+Maellewjdkkj2bek12n@users.noreply.github.com>
2024-11-21 18:17:34 +0100

    j'ai fini les menus déroulants et j'ai fait en sorte que la mini galerie soit cliquable

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,37 @@
 console.log("Script bien chargé")
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionner l'image et le menu
+    const menuToggle1 = document.getElementById('menu-toggle1');
+    const menu1 = document.getElementById('menu1');
+
+    // Ajouter un événement de clic à l'image
+    menuToggle1.addEventListener('click', () => {
+        // Basculer la classe "hidden" sur le menu
+        menu1.classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionner l'image et le menu
+    const menuToggle2 = document.getElementById('menu-toggle2');
+    const menu2 = document.getElementById('menu2');
+
+    // Ajouter un événement de clic à l'image
+    menuToggle2.addEventListener('click', () => {
+        // Basculer la classe "hidden" sur le menu
+        menu2.classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionner l'image et le menu
+    const menuToggle3 = document.getElementById('menu-toggle3');
+    const menu3 = document.getElementById('menu3');
+
+    // Ajouter un événement de clic à l'image
+    menuToggle3.addEventListener('click', () => {
+        // Basculer la classe "hidden" sur le menu
+        menu3.classList.toggle('hidden');
+    });
+});
