06db4ef13d4251f59bea87e1d143ac055ea32014
MaelleG <182913177+Maellewjdkkj2bek12n@users.noreply.github.com>
2024-11-28 13:45:45 +0100

    sauvegarde

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,11 +1,14 @@
 console.log("Script bien chargé")
 
+//pour menu déroulant en appuyant sur logo
+
+//pour modifer profil perso
 document.addEventListener('DOMContentLoaded', () => {
-        // Sélectionner l'image et le menu
+        // Sélectionner logo et menu
         const menuToggle1 = document.getElementById('menu-toggle1');
         const menu1 = document.getElementById('menu1');
 
-        // Ajouter un événement de clic à l'image
+        // Ajouter un événement de clic à logo
         menuToggle1.addEventListener('click', () => {
             // Basculer la classe "hidden" sur le menu
             menu1.classList.toggle('hidden');
@@ -16,14 +19,23 @@ document.addEventListener('DOMContentLoaded', () => {
                 menuToggle1.src = '/static/imgs/SettingsA.png' ;
             }
         });
+
+        window.showMenu = (menu4) => {
+            document.querySelectorAll('.menu').forEach(menu => {
+                menu.classList.add('hidden'); // Cacher tous les menus
+                
+            });
+            document.getElementById(menu4).classList.remove('hidden'); // Afficher le menu sélectionné
+        };
 });
 
+//pour bloquer et parler à autre utilisateurs
 document.addEventListener('DOMContentLoaded', () => {
-        // Sélectionner l'image et le menu
+        // Sélectionner logo et menu
         const menuToggle2 = document.getElementById('menu-toggle2');
         const menu2 = document.getElementById('menu2');
 
-        // Ajouter un événement de clic à l'image
+        // Ajouter un événement de clic à logo
         menuToggle2.addEventListener('click', () => {
             // Basculer la classe "hidden" sur le menu
             menu2.classList.toggle('hidden');
@@ -34,14 +46,26 @@ document.addEventListener('DOMContentLoaded', () => {
             menuToggle2.src = '/static/imgs/SettingsA.png';
         }
         });
+
+        document.addEventListener('click', () => {
+            if (!menu2.classList.contains('hidden')) {
+                menu2.classList.add('hidden');
+                menuToggle2.src = '/static/imgs/Settings.png'; // Remettre l'icône initiale
+            }
+        });
+
+        menu2.addEventListener('click', (event) => {
+            event.stopPropagation();
+        });
 });
 
+//pour filtrer les oeuvres
 document.addEventListener('DOMContentLoaded', () => {
-        // Sélectionner l'image et le menu
+        // Sélectionner logo et menu
         const menuToggle3 = document.getElementById('menu-toggle3');
         const menu3 = document.getElementById('menu3');
 
-        // Ajouter un événement de clic à l'image
+        // Ajouter un événement de clic à logo
         menuToggle3.addEventListener('click', () => {
             // Basculer la classe "hidden" sur le menu
             menu3.classList.toggle('hidden');
@@ -53,3 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         });
 });
+
+
+
+
