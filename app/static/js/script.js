3822a6e9701b80c2d32a27b6dc522ac005e7c614
MaelleG <182913177+Maellewjdkkj2bek12n@users.noreply.github.com>
2024-11-30 01:01:09 +0100

    petites modifications pas importantes

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,28 +6,71 @@ console.log("Script bien chargé")
 document.addEventListener('DOMContentLoaded', () => {
         // Sélectionner logo et menu
         const menuToggle1 = document.getElementById('menu-toggle1');
-        const switchToMenu4 = document.getElementById('switchToMenu4');
+        const switchToMenu11 = document.getElementById('switchToMenu11');
+        const switchToMenu12 = document.getElementById('switchToMenu12');
+        const switchToMenu13 = document.getElementById('switchToMenu13');
+        const switchToMenu14 = document.getElementById('switchToMenu14');
+
         const menu1 = document.getElementById('menu1');
-        const menu4 = document.getElementById('menu4');
+        const menu11 = document.getElementById('menu11');
+        const menu12 = document.getElementById('menu12');
+        const menu13 = document.getElementById('menu13');
+        const menu14 = document.getElementById('menu14');
 
         // Ajouter un événement de clic à logo
         menuToggle1.addEventListener('click', () => {
             // Basculer la classe "hidden" sur le menu
             menu1.classList.toggle('hidden');
-            menu4.classList.add('hidden');
+            menu11.classList.add('hidden');
+            menu12.classList.add('hidden');
+            menu13.classList.add('hidden');
+            menu14.classList.add('hidden');
 
-            if (!menu4.classList.contains('hidden')) {
+            if (!menu1.classList.contains('hidden')) {
                 menuToggle1.src = '/static/imgs/SettingsA.png';
-            } else if (!menu1.classList.contains('hidden')) {
+            } else if (!menu11.classList.contains('hidden')) {
+                menuToggle1.src = '/static/imgs/SettingsA.png';
+            } else if (!menu12.classList.contains('hidden')) {
+                menuToggle1.src = '/static/imgs/SettingsA.png';
+            } else if (!menu13.classList.contains('hidden')) {
+                menuToggle1.src = '/static/imgs/SettingsA.png';
+            } else if (!menu14.classList.contains('hidden')) {
                 menuToggle1.src = '/static/imgs/SettingsA.png';
             } else {
                 menuToggle1.src = '/static/imgs/Settings.png';
             }
         });
 
-        switchToMenu4.addEventListener('click', () => {
+        switchToMenu11.addEventListener('click', () => {
             menu1.classList.add('hidden');
-            menu4.classList.remove('hidden');
+            menu12.classList.add('hidden');
+            menu13.classList.add('hidden');
+            menu14.classList.add('hidden');
+            menu11.classList.remove('hidden');
+        });
+
+        switchToMenu12.addEventListener('click', () => {
+            menu1.classList.add('hidden');
+            menu11.classList.add('hidden');
+            menu13.classList.add('hidden');
+            menu14.classList.add('hidden');
+            menu12.classList.remove('hidden');
+        });
+
+        switchToMenu13.addEventListener('click', () => {
+            menu1.classList.add('hidden');
+            menu11.classList.add('hidden');
+            menu12.classList.add('hidden');
+            menu14.classList.add('hidden');
+            menu13.classList.remove('hidden');
+        });
+
+        switchToMenu14.addEventListener('click', () => {
+            menu1.classList.add('hidden');
+            menu11.classList.add('hidden');
+            menu12.classList.add('hidden');
+            menu13.classList.add('hidden');
+            menu14.classList.remove('hidden');
         });
 });
 
@@ -69,6 +112,20 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 });
 
+//pour changer les boutons quand on séléctionne une classe la partie uploade 
+const uploadgrandsboutons = document.querySelectorAll('.uploadgrandsboutons');
+    uploadgrandsboutons.forEach(button => {
+      button.addEventListener('click', () => {
+        button.classList.toggle('clicked');  // Ajoute ou enlève la classe "clicked"
+      });
+    });
 
+//pour changer les boutons quand on séléctionne un type d'art à filtrer
+const typeartboutons = document.querySelectorAll('.typeartboutons');
+typeartboutons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('clicked');  // Ajoute ou enlève la classe "clicked"
+  });
+});
 
 
